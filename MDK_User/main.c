d2c571b2558111f6a849fd7ede9c16e52851d742
young-nights <young-nights@users.noreply.github.com>
2026-05-14 15:29:34 +0800

    fix: restore TIM2 init for 1ms tick timer (was removed with GeneralTIM.c)

--- MDK_User/main.c
+++ MDK_User/main.c
@@ -5,11 +5,39 @@
 
 extern void delay_ms(uint16_t nms);
 
+/* TIM2 1ms tick timer initialization */
+static void TIM2_Init(void)
+{
+    TIM_TimeBaseInitTypeDef TIM_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    /* NVIC config */
+    NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+
+    /* TIM2: 72MHz / 71+1 = 1MHz, period 1000-1 = 1ms */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+    TIM_InitStructure.TIM_Period = 1000 - 1;
+    TIM_InitStructure.TIM_Prescaler = 71;
+    TIM_InitStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_InitStructure.TIM_RepetitionCounter = 0;
+    TIM_TimeBaseInit(TIM2, &TIM_InitStructure);
+
+    TIM_ClearFlag(TIM2, TIM_FLAG_Update);
+    TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+    TIM_Cmd(TIM2, ENABLE);
+}
+
 int main(void)
 {
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
     delay_init(72);
     Debug_USART_Init();
+    TIM2_Init();
 
     printf("\r\n========================================\r\n");
     printf("  ESP-01S Test Project\r\n");
